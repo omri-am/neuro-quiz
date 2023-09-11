11a0c487cdc4f20002dbc9ad42f819b44da67c0b
Omri Amit <omry.amit@gmail.com>
2023-09-11 21:34:51 +0300

    using Answer component

--- client/src/components/Answer.jsx
+++ client/src/components/Answer.jsx
@@ -1,6 +1,45 @@
 import React, { useState } from "react";
 
-const Answer = ({ image, answer, answerDistance, isAnswerCorrect }) => {
+const imagesPath = "../../../server/images";
+
+const highlightIncorrectLetters = (answer, correctAnswer) => {
+  const submittedWords = answer.split(" ");
+  const correctWords = correctAnswer.split(" ");
+  const highlighted = [];
+
+  for (
+    let i = 0;
+    i < Math.max(correctWords.length, submittedWords.length);
+    i++
+  ) {
+    const correctWord = correctWords[i] || "";
+    const submittedWord = submittedWords[i] || "";
+
+    for (
+      let j = 0;
+      j < Math.max(correctWord.length, submittedWord.length);
+      j++
+    ) {
+      const correctLetter = correctWord[j] || "";
+      const userLetter = submittedWord[j] || "";
+
+      if (userLetter === correctLetter) {
+        highlighted.push(userLetter);
+      } else {
+        highlighted.push(
+          <span key={`${i}_${j}`} className="incorrect-letter">
+            {userLetter}
+          </span>
+        );
+      }
+    }
+    highlighted.push(" ");
+  }
+
+  return highlighted;
+};
+
+const Answer = ({ image, answer, isAnswerCorrect }) => {
   const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseEnter = () => {
@@ -12,22 +51,25 @@ const Answer = ({ image, answer, answerDistance, isAnswerCorrect }) => {
   };
 
   return (
-    <div className="answer-container">
+    <div
+      className={`answer-container ${isHovered ? "hovered-card" : ""}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <img
-        src={image.filename}
+        src={`${imagesPath}/${image.filename}`}
         alt={image.name}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
+        style={{ maxWidth: "85%", maxHeight: "250px" }}
       />
-      {isHovered && (
-        <div className="answer-card">
-          <img src={image.filename} alt={image.name} />
+      {
+        <div>
           <p>
             <b>Expected: </b>
             {image.name}
           </p>
           <p>
-            <b>Your answer:</b> <span>{answer}</span>
+            <b>Your answer:</b>{" "}
+            <span>{highlightIncorrectLetters(answer, image.name)}</span>
           </p>
           <p>
             {isAnswerCorrect ? (
@@ -37,7 +79,7 @@ const Answer = ({ image, answer, answerDistance, isAnswerCorrect }) => {
             )}
           </p>
         </div>
-      )}
+      }
     </div>
   );
 };
--- client/src/screens/ScoresScreen.jsx
+++ client/src/screens/ScoresScreen.jsx
@@ -1,104 +1,24 @@
 import React, { useState } from "react";
-
-const imagesPath = "../../../server/images";
-
-const highlightIncorrectLetters = (answer, correctAnswer) => {
-  const submittedWords = answer.split(" ");
-  const correctWords = correctAnswer.split(" ");
-  const highlighted = [];
-
-  for (
-    let i = 0;
-    i < Math.max(correctWords.length, submittedWords.length);
-    i++
-  ) {
-    const correctWord = correctWords[i] || "";
-    const submittedWord = submittedWords[i] || "";
-
-    for (
-      let j = 0;
-      j < Math.max(correctWord.length, submittedWord.length);
-      j++
-    ) {
-      const correctLetter = correctWord[j] || "";
-      const userLetter = submittedWord[j] || "";
-
-      if (userLetter === correctLetter) {
-        highlighted.push(userLetter);
-      } else {
-        highlighted.push(
-          <span key={`${i}_${j}`} className="incorrect-letter">
-            {userLetter}
-          </span>
-        );
-      }
-    }
-    highlighted.push(" ");
-  }
-
-  return highlighted;
-};
+import Answer from "../components/Answer";
 
 const ScoresScreen = ({ score, shuffledImages, answers, handleTryAgain }) => {
   const [hoveredCardIndex, setHoveredCardIndex] = useState(null);
-  const [sortOption, setSortOption] = useState("alphabetical");
 
   const resultsGrid = (answers, correct) => {
-    return answers
+    const [sortOption, setSortOption] = useState("alphabetical");
+
+    const filteredAnswers = answers
       .filter((answer) =>
         correct ? answer.isAnswerCorrect : !answer.isAnswerCorrect
       )
       .sort((a, b) =>
-        sortOption === "accuracy"
-          ? a.answerDistance - b.answerDistance
-          : a.image.name.localeCompare(b.image.name)
-      )
-      .map((answer, index) => (
-        <div
-          key={`answer_${index}`}
-          className={`result-item ${
-            index === hoveredCardIndex ? "hovered-card" : ""
-          }`}
-          onMouseEnter={() => setHoveredCardIndex(index)}
-          onMouseLeave={() => setHoveredCardIndex(null)}
-        >
-          <img
-            src={`${imagesPath}/${answer.image.filename}`}
-            alt={answer.image.name}
-            style={{ maxWidth: "85%", maxHeight: "250px" }}
-          />
-          <p>
-            <b>Expected: </b>
-            {answer.image.name}
-          </p>
-          <p>
-            <b>Your answer:</b>{" "}
-            <span>
-              {highlightIncorrectLetters(answer.answer, answer.image.name)}
-            </span>
-          </p>
-          <p>
-            {answer.isAnswerCorrect !== null && (
-              <span
-                className={
-                  answer.isAnswerCorrect ? "correct-answer" : "incorrect-answer"
-                }
-              >
-                {answer.isAnswerCorrect ? "✓" : "✕"}
-              </span>
-            )}
-          </p>
-        </div>
-      ));
-  };
+        sortOption === "alphabetical"
+          ? a.image.name.localeCompare(b.image.name)
+          : a.answerDistance - b.answerDistance
+      );
 
-  return (
-    <div>
-      <h1>Neuroanatomy Quiz</h1>
+    return (
       <div>
-        <p>
-          Quiz completed! Final Score: {score} out of {shuffledImages.length}
-        </p>
         <div>
           <label htmlFor="sortOption">Sort answers by: </label>
           <select
@@ -110,6 +30,26 @@ const ScoresScreen = ({ score, shuffledImages, answers, handleTryAgain }) => {
             <option value="accuracy">Answers' Accuracy</option>
           </select>
         </div>
+        <p> </p>
+        {filteredAnswers.map((answer, index) => (
+          <Answer
+            key={`answer_${index}`}
+            image={answer.image}
+            answer={answer.answer}
+            isAnswerCorrect={answer.isAnswerCorrect}
+          />
+        ))}
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <h1>Neuroanatomy Quiz</h1>
+      <div>
+        <p>
+          Quiz completed! Final Score: {score} out of {shuffledImages.length}
+        </p>
         <p>
           <button onClick={handleTryAgain}>Try again</button>
         </p>
